e98d6f5a3062aa6d7908ec3af7d60171d0db352e
Stephanie V Barker <sbarker@sonomapartners.com>
2018-03-29 15:53:13 -0500

    set up webpack, started styling bookmark viewer page

---
+++ src/components/Main.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='grid'>
+                <div className='sidebar'>
+                    <div>
+                        <label htmlFor='search'>
+                            <span>Search tags</span>
+                            <input type='text' />
+                        </label>
+                    </div>
+                    <div className='tag-group'>
+                        <span>Tag Group Name</span>
+                        <ul>
+                            <li>Tag 1</li>
+                            <li>Tag 2</li>
+                            <li>Tag 3</li>
+                            <li>Tag 4</li>
+                            <li>Tag 5</li>
+                        </ul>
+                    </div>
+                    <div className='tag-group'>
+                        <span>Tag Group Name</span>
+                        <ul>
+                            <li>Tag 1</li>
+                            <li>Tag 2</li>
+                            <li>Tag 3</li>
+                            <li>Tag 4</li>
+                            <li>Tag 5</li>
+                        </ul>
+                    </div>
+                    <div className='tag-group'>
+                        <span>Tag Group Name</span>
+                        <ul>
+                            <li>Tag 1</li>
+                            <li>Tag 2</li>
+                            <li>Tag 3</li>
+                            <li>Tag 4</li>
+                            <li>Tag 5</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='main'>
+                    <div className='link-block'>
+                        <div>
+                            <h2>Title of Link</h2>
+                            <a href=''>http://url.whatever.com/part-1</a>
+                        </div>
+                        <div>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                        <div className='footer'>
+                            <ul>
+                                <li>Tag 1</li>
+                                <li>Tag 2</li>
+                                <li>Tag 3</li>
+                                <li>Tag 4</li>
+                                <li>Tag 5</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='link-block'>
+                        <div>
+                            <h2>Title of Link</h2>
+                            <a href=''>http://url.whatever.com/part-1</a>
+                        </div>
+                        <div>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                        <div className='footer'>
+                            <ul>
+                                <li>Tag 1</li>
+                                <li>Tag 2</li>
+                                <li>Tag 3</li>
+                                <li>Tag 4</li>
+                                <li>Tag 5</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='link-block'>
+                        <div>
+                            <h2>Title of Link</h2>
+                            <a href=''>http://url.whatever.com/part-1</a>
+                        </div>
+                        <div>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                        <div className='footer'>
+                            <ul>
+                                <li>Tag 1</li>
+                                <li>Tag 2</li>
+                                <li>Tag 3</li>
+                                <li>Tag 4</li>
+                                <li>Tag 5</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Main;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env', 'react'],
+                    cacheDirectory: './webpack_cache'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ]
+}
